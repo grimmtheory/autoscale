bee0625ba2030d726072066293ec9d47ed9a9d91
Jason Grimm <jasgrimm@cisco.com>
2015-04-30 12:02:22 -0400

    updates

--- devstack-setup/vbox-devstack-setup.sh
+++ devstack-setup/vbox-devstack-setup.sh
@@ -11,15 +11,13 @@ rm -rf ./stack.basrc 2>&1 /dev/null¬
 rm -rf ./devstack 2>&1 /dev/null¬
 rm -rf ./heat-templates 2>&1 /dev/null¬
 
-echo ""
-
 # Install prerequisites
 echo "# Installing dependencies..."
 sudo apt-get -y install git > /dev/null
 
 # Setup stack user
 echo "# Setting up stack user..."
-if sudo grep stack /etc/passwd > /dev/null; then
+if sudo grep stack /etc/passwd; then
         echo "Stack user already exists."
 else
         sudo useradd -d /home/stack -m stack
@@ -35,13 +33,25 @@ else
     cd /home/stack/devstack
     ./stack.sh
     ./post-stack.sh
+    echo ""
+    devstart=`head -n 1 /opt/stack/logs/stack.sh.log | awk '{ print $2 }' | cut -d . -f 1`
+    devstop=`tail -n 9 /opt/stack/logs/stack.sh.log | grep 2015 | awk '{ print $2 }' | cut -d . -f 1`
+    startdate=$(date -u -d "$devstart" +"%s")
+    enddate=$(date -u -d "$devstop" +"%s")
+    runtime=`date -u -d "0 $enddate sec - $startdate sec" +"%H:%M:%S"`
+
+    echo " -----------------------------"
+    echo " | DEVSTACK START:  $devstart |"
+    echo " | DEVSTACK STOP:   $devstop |"
+    echo " -----------------------------"
+    echo " | TOTAL RUN TIME:  $runtime |"
+    echo " -----------------------------"
+    echo ""
 fi
 EOF
 	sudo sh -c "cat ./stack.bashrc >> /home/stack/.bashrc"
 	echo "Stack user added."
 fi
-
-echo ""
 
 echo "# Adding stack user to sudoers..."
 if sudo grep stack /etc/sudoers > /dev/null; then
@@ -50,8 +60,6 @@ else
 	echo "Added stack user to sudoers"
         sudo sh -c "echo 'stack ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
 fi
-
-echo ""
 
 # Create some post stack tasks
 cat <<'EOF' > ./post-stack.sh
@@ -200,19 +208,21 @@ Q_L3_ENABLED=True
 # Q_ML2_TENANT_NETWORK_TYPE=vxlan
 
 # Enable Ceilometer - Metering Service (metering + alarming)
-enable_service ceilometer-acompute
-enable_service ceilometer-acentral
-enable_service ceilometer-anotification
-enable_service ceilometer-api
-enable_service ceilometer-alarm-notifier
-enable_service ceilometer-alarm-evaluator
+# Disabled temporarily to speed up test builds
+# enable_service ceilometer-acompute
+# enable_service ceilometer-acentral
+# enable_service ceilometer-anotification
+# enable_service ceilometer-api
+# enable_service ceilometer-alarm-notifier
+# enable_service ceilometer-alarm-evaluator
 
 # Enable Heat - Orchestration Service
-enable_service heat
-enable_service h-api
-enable_service h-api-cfn
-enable_service h-api-cw
-enable_service h-eng
+# Disabled temporarily to speed up test builds
+# enable_service heat
+# enable_service h-api
+# enable_service h-api-cfn
+# enable_service h-api-cw
+# enable_service h-eng
 
 # Images
 # Disabled temporarily to speed up builds
@@ -226,7 +236,7 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Copy files and fix permissions
 sudo cp -rf ./devstack /home/stack/
-sudo cp -rf ./heat-templates /home/stack/
+# sudo cp -rf ./heat-templates /home/stack/
 sudo chmod +x ./post-stack.sh
 sudo cp -rf ./post-stack.sh /home/stack/devstack
 sudo chown -R stack:stack /home/stack/*
